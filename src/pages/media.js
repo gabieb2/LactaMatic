52e870b9e67257e0eedfb491f6ff0236c4ef495a
Gabriel <gabibeinotti13@gmail.com>
2025-09-21 20:07:41 -0300

    style: media page style

--- src/pages/media.js
+++ src/pages/media.js
@@ -15,17 +15,20 @@ export default function Home() {
 
       <div className={styles.app}>
         <Header />
+        <main className={styles.mainContent}>
+
         <div style={{ position: "relative", width: "100%", paddingBottom: "150%", height: 0 }}>
           <iframe
             src="https://uncinnova.unc.edu.ar/servicio-de-asistencia-tecnica-y-cientifica-para-bancos-de-leche-humana/"
             style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", border: 0 }}
             allowFullScreen
             title="Nota"
-          />
+          />        
         </div>
-        
+      </main>
+      </div>
     </div>
 
-    </div>
+
   )
 }
