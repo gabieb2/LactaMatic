b87ecba09c8ec561682eb4d1f960374f7ffeb404
Gabriel <gabibeinotti13@gmail.com>
2025-09-26 11:43:21 -0300

    feat: Analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
-import { LactaTechProvider } from '../context/LactaTechContext'
-import '../styles/globals.css'
+import App from 'next/app';
+import { LactaTechProvider } from '../context/LactaTechContext';
+import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <LactaTechProvider>
-      <Component {...pageProps} />
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
     </LactaTechProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
